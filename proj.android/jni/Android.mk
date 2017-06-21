b513a2d6ff916d4361531728d748f68758255874
Wilson E. Alvarez <wilson.e.alvarez1@gmail.com>
2017-06-21 16:51:52 -0400

    Updated makefiles. Only Linux compilation was tested.
    The rest of the platforms will have to include all the directories
    listed in CMakeLists.txt

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,8 +12,58 @@ LOCAL_MODULE := MyGame_shared
 LOCAL_MODULE_FILENAME := libMyGame
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+	../../Classes/AppDelegate.cpp \
+	../../Classes/HelloWorldScene.cpp \
+	../../Classes/2DSoftShadow/ens2DSoftShadowNode.cpp \
+	../../Classes/2DSoftShadow/layer_2DSoftShadow.cpp \
+	../../Classes/2DSoftShadow/scene_2DSoftShadow.cpp \
+	../../Classes/AppDelegate.cpp \
+	../../Classes/HelloWorldScene.cpp \
+	../../Classes/break/ensBreakNode.cpp \
+	../../Classes/break/layer_break.cpp \
+	../../Classes/break/scene_break.cpp \
+	../../Classes/chooseDemoUI/chooseLayer.cpp \
+	../../Classes/chooseDemoUI/chooseScene.cpp \
+	../../Classes/colorAdjust/ensColorAdjustNode.cpp \
+	../../Classes/colorAdjust/layer_colorAdjust.cpp \
+	../../Classes/colorAdjust/scene_colorAdjust.cpp \
+	../../Classes/common/ensDefine.cpp \
+	../../Classes/common/ensFunc.cpp \
+	../../Classes/common/ensGLProgramWithUnifos.cpp \
+	../../Classes/common/ensIndexVBO.cpp \
+	../../Classes/common/ensMath.cpp \
+	../../Classes/common/ensMesh.cpp \
+	../../Classes/common/ensSimpleClasses.cpp \
+	../../Classes/ghost-like/ensGhostNode.cpp \
+	../../Classes/ghost-like/layer_ghostlike.cpp \
+	../../Classes/ghost-like/scene_ghostlike.cpp \
+	../../Classes/laser/ensLaserLayer.cpp \
+	../../Classes/laser/laserScene.cpp \
+	../../Classes/lightningBolt/ensLightningBoltNode.cpp \
+	../../Classes/lightningBolt/layer_lightningBolt.cpp \
+	../../Classes/lightningBolt/scene_lightningBolt.cpp \
+	../../Classes/normalMapped/ensNormalMappedNode.cpp \
+	../../Classes/normalMapped/layer_normalMapped.cpp \
+	../../Classes/normalMapped/scene_normalMapped.cpp \
+	../../Classes/ripple/ensRippleNode.cpp \
+	../../Classes/ripple/layer_ripple.cpp \
+	../../Classes/ripple/scene_ripple.cpp \
+	../../Classes/ripple_horizontal/ensRipple_horizontalNode.cpp \
+	../../Classes/ripple_horizontal/layer_ripple_horizontal.cpp \
+	../../Classes/ripple_horizontal/scene_ripple_horizontal.cpp \
+	../../Classes/shatter/ensShatterNode.cpp \
+	../../Classes/shatter/layer_shatter.cpp \
+	../../Classes/shatter/scene_shatter.cpp \
+	../../Classes/splash/__multiSpotLightLayer.cpp \
+	../../Classes/splash/ensSplashLayer.cpp \
+	../../Classes/splash/layer_splash.cpp \
+	../../Classes/splash/scene_splash.cpp \
+	../../Classes/tail/ensTailNode.cpp \
+	../../Classes/tail/layer_tail.cpp \
+	../../Classes/tail/scene_tail.cpp \
+	../../Classes/tinyWingsTerrain/ensTinyWingsTerrainNode.cpp \
+	../../Classes/tinyWingsTerrain/layer_tinyWingsTerrain.cpp \
+	../../Classes/tinyWingsTerrain/scene_tinyWingsTerrain.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
